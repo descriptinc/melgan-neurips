482373a911520d7c8c02e64f88867931125c43d5
Lewis Jang <panjojo.com@gmail.com>
2023-08-28 18:12:50 +0800

    update stft function to using new version of torch.

--- mel2wav/modules.py
+++ mel2wav/modules.py
@@ -61,8 +61,10 @@ class Audio2Mel(nn.Module):
             win_length=self.win_length,
             window=self.window,
             center=False,
+            return_complex=False
         )
-        real_part, imag_part = fft.unbind(-1)
+        
+        real_part, imag_part = fft[:, :, :, 0], fft[:, :, :, 1]
         magnitude = torch.sqrt(real_part ** 2 + imag_part ** 2)
         mel_output = torch.matmul(self.mel_basis, magnitude)
         log_mel_spec = torch.log10(torch.clamp(mel_output, min=1e-5))
